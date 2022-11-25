52478668b9b99399d7a5feeb09ff7457fba7e482
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-11-25 11:21:06 +0300

    no message

---
+++ app/src/main/java/com/example/temelkomutlarunite_3/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.temelkomutlarunite_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnUyg1;
+    Button btnUyg2;
+    Button btnUyg3;
+    Button btnUyg4;
+    Button btnUyg5;
+    Button btnUyg6;
+    Button btnUyg7;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnUyg1 = findViewById(R.id.btnUyg1);
+        btnUyg2 = findViewById(R.id.btnUyg2);
+        btnUyg3 = findViewById(R.id.btnUyg3);
+        btnUyg4 = findViewById(R.id.btnUyg4);
+        btnUyg5 = findViewById(R.id.btnUyg5);
+        btnUyg6 = findViewById(R.id.btnUyg6);
+        btnUyg7 = findViewById(R.id.btnUyg7);
+
+        btnUyg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Uyg1Activity.class);
+                startActivity(i);
+            }
+        });
+
+        btnUyg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Uyg2Activity.class);
+                startActivity(i);
+            }
+        });
+
+        btnUyg3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Uyg3Activity.class);
+                startActivity(i);
+            }
+        });
+
+        btnUyg4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Uyg4Activity.class);
+                startActivity(i);
+            }
+        });
+
+        btnUyg5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Uyg5Activity.class);
+                startActivity(i);
+            }
+        });
+        btnUyg6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,Uyg6Activity.class);
+                startActivity(i);
+            }
+        });
+        btnUyg7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,Uyg7Activity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
